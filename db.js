9c7f08b08c7bc8bbe2b6d2af2d25b2d70f938d25
Dev Patel <devpatel@dinksolutions.com>
2024-04-30 10:49:02 +0000

    date2

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const DbConnection = async ()=>{
     try{
-        const dbConnection = await mongoose.connect("mongodb+srv://dev:Ssaa123@cluster0.pf0qwiv.mongodb.net/Expens?retryWrites=true&w=majority&appName=Cluster0");
+        const dbConnection = await mongoose.connect("mongodb+srv://dev:Ssaa123@cluster0.pf0qwiv.mongodb.net/Expense?retryWrites=true&w=majority&appName=Cluster0");
         console.log(`Database connection successful ${dbConnection.connection.host}`)
     }
     catch(e){
