987960b85352e3fa46749468f46ab98e0147034e
Dev Patel <devpatel@dinksolutions.com>
2024-04-30 08:53:34 +0000

    dev

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const env = require('dotenv').config();
 const app = express();
+const cors=require('cors')
 const dbConnection = require('./db');
 const UserData = require("./schema");
 const port = process.env.PORT || 3000
 app.use(express.json());
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }))
 app.use(express.urlencoded({extended:true}))
 
 app.get('/allData',(req,res)=>{
